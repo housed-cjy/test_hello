08ea12612d63806cdbd0c9587b0611613143c8fb
cjy_2 <cjy773857813@163.com>
2018-10-16 17:18:05 +0800

    另外项目提交

---
+++ app/src/main/java/com/cjy/git_commit/Project2.java
@@ -0,0 +1,13 @@
+package com.cjy.git_commit;
+
+import android.util.Log;
+
+public class Project2 {
+
+    public Project2() {
+    }
+
+    public void main() {
+        Log.i("Project2", "do some");
+    }
+}
