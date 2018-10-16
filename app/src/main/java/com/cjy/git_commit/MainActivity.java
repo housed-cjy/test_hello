603dd1f396be81ce213dcfd4967e91cf4ca34186
unknown <cjy77@MSI.DEV.SPDB.COM>
2018-10-16 15:10:57 +0800

    first commit

---
+++ app/src/main/java/com/cjy/git_commit/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cjy.git_commit;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        test();
+    }
+
+    private void test() {
+        Toast.makeText(this, "xx", Toast.LENGTH_SHORT).show();
+
+    }
+}
